ab0da9449efb910b5846e4e9882fcee0438e6294
hamimDev <mdhamimhowladerasif@gmail.com>
2025-07-06 16:57:07 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://softicore-it-client.vercel.app",
-    "http://softicoreit.com"
+    "https://softicoreit.com"
   ], credentials: true
 }))
 
